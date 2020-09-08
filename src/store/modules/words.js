78604bb846abb59e0c8cd761ffa98b624d7137b7
GregoryMerlet <gregory.merlet@outlook.fr>
2020-09-08 21:52:30 +0200

    feat: change wordBanks ids to uuid strings

--- src/store/modules/words.js
+++ src/store/modules/words.js
@@ -5,37 +5,37 @@ const words = {
     state: {
         wordBanks: [
             {
-                id: 0,
+                id: '24b64c31-1ba5-4521-958b-cec4ac09ebb2',
                 title: "Actions",
                 description: "",
                 words: [],
             },
             {
-                id: 1,
+                id: '02035124-9018-432c-835f-dcd71d1e108b',
                 title: "Animals",
                 description: "You will be the monkey !",
                 words: [],
             },
             {
-                id: 2,
+                id: '14a22ba1-a20a-4c4c-a345-7bc10e3de948',
                 title: "Sayings",
                 description: "Some good old sayingsS",
                 words: [],
             },
             {
-                id: 3,
+                id: 'b6cc44dc-a900-42da-847d-3d17515c330a',
                 title: "Jobs",
                 description: "",
                 words: [],
             },
             {
-                id: 4,
+                id: '3471d60a-ead5-45a6-a0ba-cdbcd0cd8c82',
                 title: "People",
                 description: "Celebrities, Famous persons, Fictionnary persons...",
                 words: [],
             },
             {
-                id: 5,
+                id: '86d5ce49-e21b-4993-97e8-277bf41f4aa7',
                 title: "Sports",
                 description: "",
                 words: [],
